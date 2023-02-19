21270af48fb648a3bb837d7925857ccbf8741b60
pinkyrajab <81649511+pinkyrajab@users.noreply.github.com>
2023-02-19 18:46:27 +0300

    bootstrap changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import './App.css';
+import React from "react";
+import ParticlesBackground from './Components/ParticlesBackground';
+import TopNav from './Components/TopNav/TopNav';
+import About from './Components/About/About';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticlesBackground/>
+      <div>
+        <TopNav/>
+      </div>
+      <div>
+        <Router>
+          <Routes>
+              <Route path='/' element={<About/>}/>
+          </Routes>
+        </Router>
+      </div>
+      
+      
     </div>
   );
 }
---
+++ src/Components/TopNav/TopNav.js
@@ -0,0 +1,8 @@
+function topNav(){
+    return(
+        <div>
+            <button className="btn btn-white">submit</button>
+        </div>
+    )
+};
+export default topNav
